8f83bf9ea098e16c46a810fd36fbdf684f9040d8
Adam Laycock <adam@arcath.net>
2018-12-21 15:36:33 +0000

    improve coverage

--- src/clear-dead-creeps-from-memory/clear-dead-creeps-from-memory.spec.ts
+++ src/clear-dead-creeps-from-memory/clear-dead-creeps-from-memory.spec.ts
@@ -1,20 +1,26 @@
 import clearDeadCreepsFromMemory from './'
 import stubMemory from '../stub/memory'
 import stubGame from '../stub/game'
+import stubCreep from '../stub/creep'
 
 describe('Clear Dead Creeps From Memory', () => {
   it('should remove dead creeps', () => {
     stubMemory({
       creeps: {
-        'foo': {foo: 'bar'}
+        'foo': {foo: 'bar'},
+        'bar': {foo: 'bar'}
       }
     })
-    stubGame()
+    stubGame({
+      creeps: [
+        stubCreep('bar')
+      ]
+    })
 
-    expect(Object.keys(Memory.creeps).length).toBe(1)
+    expect(Object.keys(Memory.creeps).length).toBe(2)
 
     clearDeadCreepsFromMemory()
 
-    expect(Object.keys(Memory.creeps).length).toBe(0)
+    expect(Object.keys(Memory.creeps).length).toBe(1)
   })
 })
